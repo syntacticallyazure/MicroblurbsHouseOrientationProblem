ca4f5e7ecd7475488dc2bb7ca980e4b019e6e273
syntacticallyazure <95119739+syntacticallyazure@users.noreply.github.com>
2025-10-26 19:30:17 +1100

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,20 @@
 import ijson
 import json
+from shapely.geometry import shape, Point
+import requests
 
 #probably make this a .env
 # https://www.kaggle.com/datasets/joebeachcapital/australia-building-footprints
+# the data was retrieved from Kaggle, originating from Microsoft's Image Recognition AI.
 DATA_FILE = './data/Australia.geojson'
 
+# reads the file and returns all "features" aka houses as a generator.
 def feature_generator(file_path):
     with open(file_path, "r", encoding="utf-8") as f:
         for feature in ijson.items(f, "features.item"):
             yield feature
 
-# for feature in feature_generator('./data/Australia.geojson'):
-#     print(feature)
-#     break
-
-from shapely.geometry import shape, Point
-
+#when provided a lon and lat, as well as a file, check if that point falls within the boundaries dictated within the file.
 def is_point_in_feature(lon, lat, file_path):
     counter = 1
     """
@@ -31,8 +30,7 @@ def is_point_in_feature(lon, lat, file_path):
         counter = counter + 1
     return False
 
-import requests
-
+#boilerplate code to fetch from an API, extract the long and lat and run them through the above two functions.
 url = "https://www.microburbs.com.au/report_generator/api/suburb/properties"
 params = {
     "suburb": "Belmont North"
@@ -50,3 +48,7 @@ for real_estate_property in data["results"]:
     print(f"At provided long/lat: " + json.dumps(real_estate_property.get("coordinates")))
     is_point_in_feature(real_estate_property.get("coordinates")["longitude"], real_estate_property.get("coordinates")["latitude"], './data/Australia.geojson')
     break
+
+#TODO: There are multiple ways to go about doing this.
+# - You could assume that the face of the house is always the longest side. But that is not perfect.
+# - The flawless method is to determine the coordinates of the provided road and return coordinates that are closest to the house; the side that is the closest, must be the face, and the orientation would also be solved.
